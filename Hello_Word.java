670ddd72b5990078d48e6a8fed1acbc894cfdc6c
JOASTIAN98 <80014607+JOASTIAN98@users.noreply.github.com>
2021-09-19 10:16:48 -0500

    Update Hello_Word.java

--- Hello_Word.java
+++ Hello_Word.java
@@ -8,6 +8,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+Edit completo George_quintero;
+
 import static org.junit.Assert.*;
 
 /**
@@ -21,6 +23,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.helloworld,Julian", appContext.getPackageName());
+        assertEquals("com.example.helloworld_Joastian", appContext.getPackageName());
     }
 }
