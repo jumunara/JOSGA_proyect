0fac20851d2b8a0f4add4602870e1177a091b3ec
ossanti <80013590+ossanti@users.noreply.github.com>
2021-09-21 21:59:53 -0500

    Update Hello_Word.java

--- Hello_Word.java
+++ Hello_Word.java
@@ -22,7 +22,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-           assertEquals("com.example.helloworld,Joastian,Julian", appContext.getPackageName());
+           assertEquals("com.example.helloworld,Joastian,Julian,Oscar", appContext.getPackageName());
 
     }
 }
